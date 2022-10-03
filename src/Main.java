6935236f99051ab48feb5cd774e1f2948077b9d1
grisha <norsulfasol@gmail.com>
2022-10-03 13:06:02 +0300

    ДЗ 5 Задание 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание_1");
+        int clientOS=0;
+        if (clientOS==0) {
+        System.out.println("Установите версию приложения для iOS по ссылке!");
+        } else if (clientOS==1) {System.out.println("Установите версию приложения для Android по ссылке!");
+        } else { System.out.println("Не верное значение, повторите ввод!");
+        }
+      }
+   }
+
