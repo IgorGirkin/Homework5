b4a0b513bd43686b43c852d2da04b178d9b50c2d
grisha <norsulfasol@gmail.com>
2022-10-03 14:11:09 +0300

    ДЗ 5 Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задание_1");
-        int clientOS=0;
-        if (clientOS==0) {
-        System.out.println("Установите версию приложения для iOS по ссылке!");
-        } else if (clientOS==1) {System.out.println("Установите версию приложения для Android по ссылке!");
-        } else { System.out.println("Не верное значение, повторите ввод!");
-
+        System.out.println("Задание_2");
+        int clientDeviceYear = 2010;
+        int clientOS = 1;
+        if (clientOS == 0&&clientDeviceYear<2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по " +
+                        "ссылке!");
+            } else  if (clientOS == 0)
+            {
+                System.out.println("Установите версию приложения для iOS по ссылке!");
+             }
+        else if (clientOS == 1&&clientDeviceYear<2015) {
+            System.out.println("Установите облегченную версию приложения для Android по" +
+                    " ссылке!");
+        } else if (clientOS==1) {
+            System.out.println("Установите версию приложения для Android по " +
+                    "ссылке!");
+        } else {
+            System.out.println("Значение не верно, введите друго значение!");
         }
-      }
-   }
+    }
+}
 
