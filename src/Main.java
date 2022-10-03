85b8f45e896da1399079edf15e895618a18ab668
grisha <norsulfasol@gmail.com>
2022-10-03 14:36:08 +0300

    ДЗ 5 Задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задание_2");
-        int clientDeviceYear = 2010;
-        int clientOS = 1;
-        if (clientOS == 0&&clientDeviceYear<2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по " +
-                        "ссылке!");
-            } else  if (clientOS == 0)
-            {
-                System.out.println("Установите версию приложения для iOS по ссылке!");
-             }
-        else if (clientOS == 1&&clientDeviceYear<2015) {
-            System.out.println("Установите облегченную версию приложения для Android по" +
-                    " ссылке!");
-        } else if (clientOS==1) {
-            System.out.println("Установите версию приложения для Android по " +
-                    "ссылке!");
-        } else {
-            System.out.println("Значение не верно, введите друго значение!");
+        System.out.println("Задание_3");
+        int year = 2024;
+        if(((year%4==0)&&(year%100!=0))||(year%400==0)) {
+                System.out.println(year+" год является високосным!");
+            } else {
+            System.out.println(year+" год не является високосным!");
         }
     }
 }
