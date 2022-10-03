bface2c7c222e02061d89e5cee9ae126e6529f32
grisha <norsulfasol@gmail.com>
2022-10-03 15:05:29 +0300

    ДЗ 5 Задание 4

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задание_3");
-        int year = 2024;
-        if(((year%4==0)&&(year%100!=0))||(year%400==0)) {
-                System.out.println(year+" год является високосным!");
-            } else {
-            System.out.println(year+" год не является високосным!");
+        System.out.println("Задание_4");
+        int deliveryDistance = 120;
+        if(deliveryDistance<=20) {
+                System.out.println("Потребуется дней: 1");
+            } else if (deliveryDistance>=20&&deliveryDistance<=60) {
+            System.out.println("Потребуется дней: 2" );
+        } else if (deliveryDistance>60&&deliveryDistance<=100) {
+            System.out.println("Потребуется дней: 3" );
+        } else { System.out.println("С Вами свяжется менеджер, ожидайте." );
         }
     }
 }
